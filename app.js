a7b1498b92b7539a4efdef3d65b91beeab8cb41d
furqan <furqan.azeem369@gmail.com>
2023-10-18 21:58:03 +0500

    code added

---
+++ app.js
@@ -0,0 +1,448 @@
+
+var mobile = {
+  vivo: {
+    X50_Pro: {
+      img: './img/Vivo X50 Pro.png',
+      modelName: 'Vivo X50 Pro',
+      displaySize: '6.56 inches',
+      ram: '8GB',
+      storage: '256GB',
+      camera: 'Quad 48MP + 13MP ',
+      price: "$599.99"
+    },
+    V21e: {
+      img: 'https://hamariweb.com/mobiles/LargeImages/5896_01.jpg',
+      modelName: 'Vivo V21e',
+      displaySize: '6.44 inches',
+      ram: '8GB',
+      storage: '128GB',
+      camera: 'Dual 64MP + 8MP',
+      price: "$399.99"
+
+    },
+    Y73s: {
+      img: 'https://cdn11.bigcommerce.com/s-g61ujcw369/images/stencil/1280x1280/products/2758/15158/WeChat_Image_20221031000514__03328.1667167610.jpg?c=1?imbypass=on',
+      modelName: 'Vivo Y73s',
+      displaySize: '6.44 inches',
+      ram: '8GB',
+      storage: '128GB',
+      camera: 'Triple 48MP + 8MP',
+      price: "$329.99"
+    },
+    S1_Pro: {
+      img: 'https://vmart.pk/wp-content/uploads/2020/01/S1-Pro-8GB-128GB-White-Front.jpg',
+      modelName: 'Vivo S1 Pro',
+      displaySize: '6.38 inches',
+      ram: '8GB',
+      storage: '128GB',
+      camera: 'Quad 48MP + 8MP',
+      price: "$349.99"
+    },
+    Y20: {
+      img: 'https://shopon.pk/images/thumbnails/734/600/detailed/42/vivo-Y20-Dawn-White-7.jpg',
+      modelName: 'Vivo Y20',
+      displaySize: '6.51 inches',
+      ram: '4GB',
+      storage: '64GB',
+      camera: 'Triple 13MP + 2MP',
+      price: "$199.99"
+    },
+    Z1x: {
+      img: 'https://www.notebookcheck.net/uploads/tx_nbc2/VivoZ1x__1_.JPG',
+      modelName: 'Vivo Z1x',
+      displaySize: '6.38 inches',
+      ram: '6GB',
+      storage: '128GB',
+      camera: 'Triple 48MP + 8MP',
+      price: "$289.99"
+    }
+  },
+  oppo: {
+    A9: {
+      img: 'https://www.laptopsdirect.co.uk/Images/CPH1941GREEN_1_Supersize.png?width=650&height=650&v=2',
+      modelName: "Oppo A9",
+      displaySize: "6.5 inches",
+      ram: "6 GB",
+      storage: "128 GB",
+      camera: "48 MP + 8 MP",
+      price: "$250"
+    },
+    Reno_3_Pro: {
+      img: 'https://m.media-amazon.com/images/I/51K0tLgr5oL._AC_UF1000,1000_QL80_.jpg',
+      modelName: "Oppo Reno 3 Pro",
+      displaySize: "6.4 inches",
+      ram: "8 GB",
+      storage: "128 GB",
+      camera: "64 MP + 13 MP ",
+      price: "$400"
+    },
+    Find_X2_Pro: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-42531/oppofindx2pro.jpg',
+      modelName: "Oppo Find X2 Pro",
+      displaySize: "6.7 inches",
+      ram: "12 GB",
+      storage: "512 GB",
+      camera: "48 MP + 13 MP",
+      price: "$1100"
+    },
+    F15: {
+      img: 'https://fdn2.gsmarena.com/vv/pics/oppo/oppo-f15-cph2001-2.jpg',
+      modelName: "Oppo F15",
+      displaySize: "6.4 inches",
+      ram: "8 GB",
+      storage: "128 GB",
+      camera: "48 MP + 8 MP",
+      price: "$300"
+    },
+    A52: {
+      img: 'https://www.purchase.com.pk/wp-content/uploads/2021/12/oppo-a52.jpg',
+      modelName: "Oppo A52",
+      displaySize: "6.5 inches",
+      ram: "4 GB",
+      storage: "128 GB",
+      camera: "12 MP + 8 MP",
+      price: "$200"
+    },
+    Reno_4_Pro: {
+      img: 'https://www.oppo.com/content/dam/oppo/common/mkt/v2-2/navigation/reno-series/reno4-pro/4g/Reno4%20Pro-Gaudi4G-navigation-Black-v2.png',
+      modelName: "Oppo Reno 4 Pro",
+      displaySize: "6.5 inches",
+      ram: "8 GB",
+      storage: "256 GB",
+      camera: "48 MP + 8 MP",
+      price: "$550"
+    }
+  },
+  samsung: {
+    Galaxy_S21: {
+      img: 'https://thumbs.dreamstime.com/b/smart-phone-samsung-galaxy-s-fe-front-back-sides-official-white-color-background-realistic-vector-kiev-ukraine-april-new-247980208.jpg',
+      modelName: "Samsung Galaxy S21",
+      displaySize: "6.2 inches",
+      ram: "8 GB",
+      storage: "128 GB",
+      camera: "12 MP + 64 MP",
+      price: "$799.99"
+    },
+    Galaxy_S21_Ultra: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-76697/galaxys21_ultra.jpg',
+      modelName: "Samsung Galaxy S21 Ultra",
+      displaySize: "6.8 inches",
+      ram: "12 GB",
+      storage: "256 GB",
+      camera: "108 MP + 12 MP",
+      price: "$1199.99"
+    },
+    Galaxy_A32: {
+      img: 'https://www.eastasiaeg.com/media/catalog/product/cache/7cc761d78ccccdcc12015db63d803651/1/-/1-009709.gif',
+      modelName: "Samsung Galaxy A32",
+      displaySize: "6.4 inches",
+      ram: "6 GB",
+      storage: "128 GB",
+      camera: "64 MP + 8 MP",
+      price: "$279.99"
+    },
+    Galaxy_A52: {
+      img: 'https://myshop.pk/pub/media/catalog/product/cache/26f8091d81cea4b38d820a1d1a4f62be/s/a/samsung-a52-_myshop-pk-5.jpg',
+      modelName: "Samsung Galaxy A52",
+      displaySize: "6.5 inches",
+      ram: "6 GB",
+      storage: "128 GB",
+      camera: "64 MP + 12 MP",
+      price: "$349.99"
+    },
+    Galaxy_Note_20_Ultra: {
+      img: 'https://images.anandtech.com/doci/16069/27_galaxynote20ultra_mysticwhite_back_with_pen_678x452.jpg',
+      modelName: "Samsung Galaxy Note 20 Ultra",
+      displaySize: "6.9 inches",
+      ram: "12 GB",
+      storage: "256 GB",
+      camera: "108 MP + 12 MP",
+      price: "$1299.99"
+    },
+    Galaxy_Z_Fold_3: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-99814/Samsung-Galaxy-Z-Fold3-5G-Yoast-image-packshot-review.jpg',
+      modelName: "Samsung Galaxy Z Fold 3",
+      displaySize: "7.6 inches (Main), 6.2 inches (Cover)",
+      ram: "12 GB",
+      storage: "256 GB",
+      camera: "12 MP (Main)",
+      price: "$1,799.99",
+    },
+    Galaxy_Note_20: {
+      img: 'https://png.pngtree.com/png-clipart/20200813/ourlarge/pngtree-samsung-galaxy-note-20-png-image_2325162.jpg',
+      modelName: "Samsung Galaxy Note 20",
+      displaySize: "6.7 inches",
+      ram: "8 GB",
+      storage: "256 GB",
+      camera: "12 MP (Main), 12 MP",
+      price: "$999.99"
+    },
+
+    Galaxy_S20_FE: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-70557/galaxys20fe_front_back_white-1.jpg',
+      modelName: 'Samsung Galaxy S20 FE',
+      displaySize: '6.5 inches',
+      ram: '6GB',
+      storage: '128GB',
+      camera: '12MP + 8MP + 12MP',
+      price: '$599.99'
+
+    }
+  },
+  infinix: {
+    Note_10_Pro: {
+      img: 'https://img.giznext.com/assets/model/16/4677/infinix-mobiles-infinix-note-10-pro-13-1634143411.jpg',
+      modelName: 'Infinix Note 10 Pro',
+      displaySize: '6.95 inches',
+      ram: '8 GB',
+      storage: '128 GB',
+      camera: '64 MP + 8 MP',
+      price: "$250"
+    },
+    Zero_X_Pro: {
+      img: 'https://le.co.ke/wp-content/uploads/2022/04/thumb_547795_default_big.png',
+      modelName: 'Infinix Zero X Pro',
+      displaySize: '6.67 inches',
+      ram: '8 GB',
+      storage: '256 GB',
+      camera: '108 MP + 8 MP',
+      price: "$300"
+    },
+    Hot_10: {
+      img: 'https://fdn2.gsmarena.com/vv/pics/infinix/infinix-hot10-play-1.jpg',
+      modelName: 'Infinix Hot 10',
+      displaySize: '6.78 inches',
+      ram: '6 GB',
+      storage: '128 GB',
+      camera: '16 MP + 2 MP',
+      price: "$150"
+    },
+    Smart_5A: {
+      img: 'https://m.media-amazon.com/images/I/51OTnhkwEnL._AC_UF1000,1000_QL80_.jpg',
+      modelName: 'Infinix Smart 5A',
+      displaySize: '6.52 inches',
+      ram: '2 GB',
+      storage: '32 GB',
+      camera: '8 MP + AI Lens',
+      price: "$100"
+    },
+    Hot_10T: {
+      img: 'https://www.devicespecifications.com/images/model/8ead563c/320/main.jpg',
+      modelName: 'Infinix Hot 10T',
+      displaySize: '6.82 inches',
+      ram: '4 GB',
+      storage: '64 GB',
+      camera: '48 MP + 2 MP',
+      price: "$130"
+    },
+    S5_Pro: {
+      img: 'https://www.imei.info/media/t/gsm-cache/u/K/kyIRbz-d/infinix-s5-pro.jpg',
+      modelName: 'Infinix S5 Pro',
+      displaySize: '6.53 inches',
+      ram: '6 GB',
+      storage: '128 GB',
+      camera: '48 MP + 2 MP',
+      price: "$180"
+    }
+  },
+  google: {
+    Pixel_6: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-96650/Google-Pixel-6-Pro-featured-image-packshot-review.jpg',
+      modelName: 'Google Pixel 6',
+      displaySize: '6.4 inches',
+      ram: '8 GB',
+      storage: '128 GB',
+      camera: 'Dual 50 MP',
+      price: '$699'
+    },
+    Pixel_6_Pro: {
+      img: 'https://www.androidguys.com/wp-content/uploads/2021/10/pixel_6_white.png',
+      modelName: 'Google Pixel 6 Pro',
+      displaySize: '6.7 inches',
+      ram: '12 GB',
+      storage: '256 GB',
+      camera: 'Triple 50 MP',
+      price: '$899'
+    },
+    Pixel_5a: {
+      img: 'https://sopiguard.com/cdn/shop/products/pixel53mmtwt_c707cd24-ba8c-45ff-86fe-d751a7cb2a87.jpg?v=1631955691',
+      modelName: 'Google Pixel 5a',
+      displaySize: '6.34 inches',
+      ram: '6 GB',
+      storage: '128 GB',
+      camera: 'Dual 12.2 MP',
+      price: '$449'
+    },
+    Pixel_5: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-59199/google_pixel_5_frontback.jpeg',
+      modelName: 'Google Pixel 5',
+      displaySize: '6.0 inches',
+      ram: '8 GB',
+      storage: '128 GB',
+      camera: 'Dual 12.2 MP',
+      price: '$599'
+    },
+    Pixel_4a_5G: {
+      img: 'https://cdn.dxomark.com/wp-content/uploads/medias/post-35617/Clearly-White-Pixel-4-Front_Back.jpg',
+      modelName: 'Google Pixel 4a 5G',
+      displaySize: '6.2 inches',
+      ram: '6 GB',
+      storage: '128 GB',
+      camera: 'Dual 12.2 MP',
+      price: '$499'
+    },
+    Pixel_4a: {
+      img: 'https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/10/15/15/google-pixel-4-colours-1510.jpg?width=640&auto=webp&quality=100&crop=1500%3A1000%2Csmart',
+      modelName: 'Google Pixel 4a',
+      displaySize: '5.81 inches',
+      ram: '6 GB',
+      storage: '128 GB',
+      camera: '12.2 MP',
+      price: '$349'
+    }
+  },
+
+}
+
+
+var company = document.getElementById('company')
+var brand = document.getElementById('brand')
+var allPhone = document.getElementById('allPhone')
+
+company.innerHTML = `<option disabled selected value = 'Select Company'>Select Company</option>`
+brand.innerHTML = `<option disabled selected value = 'Select Model'>Select Model</option>`
+
+
+
+for (var key in mobile) {
+  company.innerHTML += `
+   <option value = "${key}"> ${key.toUpperCase()} </option>
+   `
+
+   for (var key2 in mobile[key]) {
+    // console.log(mobile[key][key2])
+    allPhone.innerHTML += `
+    <div class="col  mb-2">
+    <div class="card"style="width: 19rem;">
+    <img src="${mobile[key][key2].img}"  height="200px" style= "object-fit:contain; margin-top:10px;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${mobile[key][key2].modelName}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary"> <span class="color-change"> RAM: </span> ${mobile[key][key2].ram}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${mobile[key][key2].storage}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Display: </span> ${mobile[key][key2].displaySize}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Camera: </span> ${mobile[key][key2].camera}</h6>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Price: </span> ${mobile[key][key2].price}</h6>
+        
+      </div>
+    </div>
+  </div>
+    `
+  }
+
+}
+
+
+function onCompanyChange() {
+  brand.innerHTML = "";
+  brand.innerHTML = `<option disabled selected>Select Model</option>`;
+
+  for (var key in mobile[company.value]) {
+    // console.log(key)
+    brand.innerHTML += `
+      <option value="${key}">${key.toUpperCase()}</option>
+    `
+  }
+}
+
+
+
+
+
+
+
+function filterPhone(){
+
+  if(company.value != 'Select Company' && brand.value != 'Select Model'){
+  var phoneData = mobile[company.value][brand.value];
+   allPhone.innerHTML = `
+   <div class="filter">
+   <div class="filter-img">
+     <img  src="${phoneData.img}" alt="">
+   </div>
+   <div class="filter-body">
+     <h3 class="model-name" >${phoneData.modelName}</h3>
+     <h6 class="ram"><span class="color-change"> RAM: </span> ${phoneData.ram}</h6>
+     <h6 class="storage"><span class="color-change"> Storage: </span> ${phoneData.storage}</h6>
+     <h6 class="display"><span class="color-change"> DisplaySize: </span> ${phoneData.displaySize}</h6>
+     <h6 class="camera"><span class="color-change"> Camera: </span> ${phoneData.camera}</h6>
+     <h6 class="price"><span class="color-change"> Price: </span> ${phoneData.price}</h6>
+   </div>
+ </div>
+   `
+   
+   // `
+//     <div class="col">
+//   <div class="card"  style="width: 18rem;">
+//   <img src="${phoneData.img}"  height="200px" style= "object-fit:contain; margin-top:10px;" class="card-img-top" alt="...">
+//     <div class="card-body">
+//       <h5 class="card-title">${phoneData.modelName}</h5>
+//       <h6 class="card-subtitle mb-2 text-body-secondary"> <span class="color-change"> RAM: </span> ${phoneData.ram}</h6>
+//       <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${phoneData.storage}</h6>
+//       <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Display: </span> ${phoneData.displaySize}</h6>
+//       <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Camera: </span> ${phoneData.camera}</h6>
+//       <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Price: </span> ${phoneData.price}</h6>
+      
+
+            
+     
+//     </div>
+//   </div>
+// </div>
+
+//   `
+  }
+
+  else{
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Please Select Company & Model',
+     
+    })
+  }
+}
+
+
+function showAll(){
+  if(company.value != 'Select Company' && brand.value != 'Select Model'){
+  allPhone.innerHTML = "";
+  for (var key in mobile) {
+   
+  
+     for (var key2 in mobile[key]) {
+    
+      allPhone.innerHTML += `
+      <div class="col  mb-2">
+      <div class="card"style="width: 19rem;">
+      <img src="${mobile[key][key2].img}"  height="200px" style= "object-fit:contain; margin-top:10px;" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${mobile[key][key2].modelName}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary"> <span class="color-change"> RAM: </span> ${mobile[key][key2].ram}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${mobile[key][key2].storage}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Display: </span> ${mobile[key][key2].displaySize}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Camera: </span> ${mobile[key][key2].camera}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Storage: </span> ${mobile[key][key2].storage}</h6>
+          <h6 class="card-subtitle mb-2 text-body-secondary"><span class="color-change">Price: </span> ${mobile[key][key2].price}</h6>
+          
+        </div>
+      </div>
+    </div>
+      `
+     
+    }
+  
+  }
+  
+}
+}
+
